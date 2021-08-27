5a823e653052784230d5cb83e02d7df708af500d
Peter Frank Csoor <csoorpeterfrank@gmail.com>
2021-08-27 23:10:47 +0200

    update work component for legged in users

--- src/components/Work.js
+++ src/components/Work.js
@@ -8,21 +8,24 @@ export default function Work() {
             {
                 localStorage.getItem('user-info') ? 
                 <>
-                    <h3>Manage projects</h3>
-                    <Link to="/add-project">Add project</Link>
-                    <Link to="/update-project">Update project</Link>
+                    <div className="max-w-md bg-white rounded-xl  mx-auto shadow-md px-3 py-6">
+                        <h3 className="font-semibold text-xl pb-1 text-center">What would you like to do?</h3>
+                        <h3 className="font-semibold mt-4">Manage projects</h3>
+                        <Link to="/add-project">Add project</Link><br />
+                        <Link to="/update-project">Update project</Link>
 
-                    <h3>Manage technologies</h3>
-                    <Link to="/add-project">Add technology</Link>
-                    <Link to="/update-project">Update technology</Link>
+                        <h3 className="font-semibold mt-4">Manage technologies</h3>
+                        <Link to="/add-project">Add technology</Link><br />
+                        <Link to="/update-project">Update technology</Link>
 
-                    <h3>Manage categories</h3>
-                    <Link to="/add-project">Add category</Link>
-                    <Link to="/update-project">Update category</Link>
+                        <h3 className="font-semibold mt-4">Manage categories</h3>
+                        <Link to="/add-project">Add category</Link><br />
+                        <Link to="/update-project">Update category</Link>
+                    </div>
+                    
                 </>
-                :
-                <>
-                </>
+                : null
+                
             }
         </div>
     )
